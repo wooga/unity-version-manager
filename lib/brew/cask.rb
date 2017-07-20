19c154b5fcec5740aa91a5f8bead337c5a79ad7e
Manfred Endres <manfred.endres@wooga.net>
2017-07-20 17:01:36 +0200

    Fix rubocop issues

--- lib/brew/cask.rb
+++ lib/brew/cask.rb
@@ -6,8 +6,8 @@ module Brew
     def search pattern
       results = []
 
-      Open3.popen3("brew cask search #{pattern}") {|i,o,e,t|
-        results = o.read.chomp.lines.map { |i| i.chomp }
+      Open3.popen3("brew cask search #{pattern}") {|_stdin,stdout,_stderr,_thread|
+        results = stdout.read.chomp.lines.map { |i| i.chomp }
       }
 
       results.select {|item| !item.start_with? "==>" }
@@ -16,8 +16,8 @@ module Brew
     def list
       results = []
 
-      Open3.popen3("brew cask list") { |i,o,e,t|
-        results = o.read.chomp.lines.map { |i| i.chomp }
+      Open3.popen3("brew cask list") { |_stdin,stdout,_stderr,_thread|
+        results = stdout.read.chomp.lines.map { |i| i.chomp }
       }
 
       results
